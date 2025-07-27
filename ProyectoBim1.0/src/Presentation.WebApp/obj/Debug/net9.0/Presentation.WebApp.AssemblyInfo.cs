95547b70aca0f05ce713dc397840ad812e29d01e
GeraFVM <gerafvm@outlook.com>
2025-07-27 16:39:53 -0600

    ProyectoBIM 1.0
    Se agrega funcion de confirmacion al eliminar registros

--- ProyectoBim1.0/src/Presentation.WebApp/obj/Debug/net9.0/Presentation.WebApp.AssemblyInfo.cs
+++ ProyectoBim1.0/src/Presentation.WebApp/obj/Debug/net9.0/Presentation.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65bc8849e919dde8147cfda24d25c0487dde8a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
