57859af1b179c8c2c38601b033663a3841ee8515
GeraFVM <gerafvm@outlook.com>
2025-07-27 16:45:00 -0600

    Ya no se que paso pero jalo

--- ProyectoBim1.0/src/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ ProyectoBim1.0/src/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65bc8849e919dde8147cfda24d25c0487dde8a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95547b70aca0f05ce713dc397840ad812e29d01e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
